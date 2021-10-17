acfc2498bf50cdd473e3cbc3d52ef8b90b38fa65
Denis Sharapov <denisharapov@yandex.ru>
2021-10-17 18:51:34 +0700

    Initial Commit

---
+++ Yoga/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yoga
+//
+//  Created by National Team on 17.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
